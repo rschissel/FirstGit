8b88a18702d9588cfe8dd1845b3bbaf250f7b3f4
ryan-schissel <ryan.schissel@yahoo.com>
2016-08-30 16:28:18 -0500

    Added another message

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -17,6 +17,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Hello java");
         System.out.println("Hello class");
+        System.out.println("Hello advanced java");
     }
     
 }
